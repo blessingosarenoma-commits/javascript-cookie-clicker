a08dbe30f03f6454308603dd589ea5a6b6a0b271
Osarenoma Blessing Dick <blessingosarenoma@gmail.com>
2025-09-19 22:58:58 +0100

    clicker

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let cookie = 0;
 const cookieElement = document.getElementById("cookie");
 const countElement = document.getElementById("count");
 cookieElement.addEventListener("click", function () {
-  cookie = cookie + 1;
+  cookie = cookie ++ 1;
   countElement.textContent = cookies;
 });
